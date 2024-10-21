a9ddd32a433b278a0099f53e062ba990df6a79de
charan <156230258+kotlasaicharan@users.noreply.github.com>
2024-10-21 13:47:30 +0530

    Update dataset_getter.py

--- dataset_getter.py
+++ dataset_getter.py
@@ -7,5 +7,32 @@ val.to_json("val.json", orient="records", lines=True)
 import json
 from datasets import load_dataset
 dataset = load_dataset("json", data_files={"train": "train.json", "validation": "val.json"})
-
 dataset
+
+------
+def format_example(row : dict):
+    prompt = dedent(
+        f"""
+        "Translate the given urdu poem to english, Provide only the translation, without any additional comments or explanations :"
+        {row["Urdu"]}
+        """
+    )
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a great translator",
+            },
+        {
+            "role": "user",
+            "content": prompt,
+        },
+        {
+            "role": "assistant",
+            "content": row["orginal_eng"],
+        }
+    ]
+
+    return tokenizer.apply_chat_template(messages, tokenize=False)
+
+df["text"] = df.apply(format_example, axis=1)
+df.head()
